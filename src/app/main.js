38fa3b59fa53e36869d0002d15ddc67fe61b77b3
Kenneth McComas <Kenneth.McComas@elections.ca>
2015-01-29 10:57:40 -0500

    updated template

--- src/app/main.js
+++ src/app/main.js
@@ -1,17 +1,25 @@
 /*global document, define, console, alert*/
 define([
+	'dijit/Dialog',
 	'dijit/form/Button',
-	"esri/map",
-	"esri/layers/FeatureLayer",
+	'esri/map',
+	'esri/layers/FeatureLayer',
 	'dojo/domReady!'
-], function (Button, Map, FeatureLayer) {
+], function (Dialog, Button, Map, FeatureLayer) {
 	"use strict";
-	var myButton, map, secureLayer;
+	var myButton, map, secureLayer, dialog;
+
+	dialog = new Dialog({
+		title: "About",
+		content: "A secure service was added to the demo application to illustrate the templates correct use of the ESRI init.js.  If the ESRI init.js was not implemented correctly the esri/kernel would not be populated with all its attributes. As a result of an incorrectly loaded esri/kernel a password prompt would not popup for a secure service.",
+		style: "width: 300px"
+	});
 
 	myButton = new Button({
-		label: "Button",
+		label: "About",
 		onClick: function () {
-			alert("Hello World");
+			//alert();
+			dialog.show();
 		}
 	}, "ButtonDiv").startup();
 
@@ -22,7 +30,10 @@ define([
 		basemap: "topo"
 	});
 
-	//add the secure service - token is required
+	// A secure service was added to the demo application to illustrate the templates correct use of the ESRI init.js.  
+	// If the ESRI init.js was not implemented correctly the esri/kernel would not be populated with all its attributes. 
+	// As a result of an incorrectly loaded esri/kernel a password prompt would not popup for a secure service.
+	// Use the following credentials to login: Username user1 Password user1
 	secureLayer = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/SaveTheBay/FeatureServer/0", {
 		mode: FeatureLayer.MODE_ONDEMAND,
 		outFields: ["*"]
